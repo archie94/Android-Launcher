7dbe9c0b156ac9e845018f190b8fd9f4e5d6fba3
Arka Prava Basu <arkaprava94@gmail.com>
2015-08-28 00:28:08 +0530

    Adding widgets functionality added

--- gen/com/example/launcherx/R.java
+++ gen/com/example/launcherx/R.java
@@ -25,25 +25,27 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int content=0x7f080003;
-        public static final int drawer=0x7f080001;
-        public static final int handle=0x7f080002;
-        public static final int home_view=0x7f080000;
-        public static final int icon_image=0x7f080004;
-        public static final int icon_text=0x7f080005;
+        public static final int APP_WIDGET_HOST_ID=0x7f050000;
+        public static final int REQUEST_PICK_APPWIDGET=0x7f050001;
+        public static final int action_settings=0x7f050008;
+        public static final int content=0x7f050005;
+        public static final int drawer=0x7f050003;
+        public static final int handle=0x7f050004;
+        public static final int home_view=0x7f050002;
+        public static final int icon_image=0x7f050006;
+        public static final int icon_text=0x7f050007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int item=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
@@ -67,10 +69,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/example/launcherx/MainActivity.java
+++ src/com/example/launcherx/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.launcherx;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.appwidget.AppWidgetHost;
+import android.appwidget.AppWidgetHostView;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProviderInfo;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -11,6 +16,8 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnLongClickListener;
 import android.widget.GridView;
 import android.widget.RelativeLayout;
 import android.widget.SlidingDrawer;
@@ -23,6 +30,12 @@ public class MainActivity extends Activity
 	DrawerAdapter drawerObj;
 	SlidingDrawer slidingDrawer;
 	RelativeLayout homeView;
+	
+	AppWidgetManager appWidgetManager;
+	AppWidgetHost appWidgetHost;
+	int REQUEST_CREATE_APPWIDGET=900;//random 
+	
+	
 	class Pack
 	{
 		Drawable icon;
@@ -32,13 +45,20 @@ public class MainActivity extends Activity
 	PackageManager pm;/*PackageManager class is used for retrieving informations related to the application 
 	packages currently installed on the device*/
 	
+	
 	static boolean appLaunchable=true;
+	
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        appWidgetManager=AppWidgetManager.getInstance(this);
+        appWidgetHost=new AppWidgetHost(this,R.id.APP_WIDGET_HOST_ID);
+        
+        
         drawGrid=(GridView)findViewById(R.id.content);
         slidingDrawer=(SlidingDrawer)findViewById(R.id.drawer);
         homeView=(RelativeLayout)findViewById(R.id.home_view);
@@ -55,6 +75,20 @@ public class MainActivity extends Activity
 				// TODO Auto-generated method stub
 				appLaunchable=true;
 				
+			}
+        	
+        });
+        
+        //On long pressing our homeView we want to open widget list
+        homeView.setOnLongClickListener(new OnLongClickListener()
+        {
+
+			@Override
+			public boolean onLongClick(View v) 
+			{
+				// TODO Auto-generated method stub
+				selectWidget();
+				return false;
 			}
         	
         });
@@ -106,5 +140,126 @@ public class MainActivity extends Activity
 		}
     	
     }
+    
+    //--------------------------------METHODS FOR WIDGET---------------------------------------------------------------------
+    
+    void selectWidget() 
+    {
+        int appWidgetId = this.appWidgetHost.allocateAppWidgetId();//allocate resources for a widget instance 
+        
+        //this activity will allow us to select which widget we want to add 
+        Intent pickIntent = new Intent(AppWidgetManager.ACTION_APPWIDGET_PICK);
+        pickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        addEmptyData(pickIntent);
+        startActivityForResult(pickIntent, R.id.REQUEST_PICK_APPWIDGET);
+    }
+    void addEmptyData(Intent pickIntent) 
+    {
+    	/*The Widget API supports that you merge custom widgets of your application with the installed ones. 
+    	 * But if you don’t add anything, the Activity that shows the list of widgets to the user crashes 
+    	 * with a NullPointerException. The addEmptyData() method above adds some dummy data to avoid this bug.
+    	 */
+        ArrayList customInfo = new ArrayList();
+        pickIntent.putParcelableArrayListExtra(AppWidgetManager.EXTRA_CUSTOM_INFO, customInfo);
+        ArrayList customExtras = new ArrayList();
+        pickIntent.putParcelableArrayListExtra(AppWidgetManager.EXTRA_CUSTOM_EXTRAS, customExtras);
+    };
+    
+    
+    /*If the user successfully selects a widget from the list (he didn’t pressed “back”), 
+     * it will return an OK to you as an activity result. The data for this result contains the widget ID. 
+     * Use it to retrieve the AppWidgetProviderInfo to check if the widget requires any configuration (some widgets does need). 
+     * If it requires, you need to launch the activity to configure the widget.(non-Javadoc)
+     * @see android.app.Activity#onActivityResult(int, int, android.content.Intent)
+    */
+    
+    
+    
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) 
+    {
+        if (resultCode == RESULT_OK ) 
+        {
+            if (requestCode == R.id.REQUEST_PICK_APPWIDGET) 
+            {
+                configureWidget(data);
+            }
+            else if (requestCode == REQUEST_CREATE_APPWIDGET) 
+            {
+                createWidget(data);
+            }
+        }
+        else if (resultCode == RESULT_CANCELED && data != null) 
+        {
+            int appWidgetId = data.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, -1);
+            if (appWidgetId != -1) 
+            {
+                appWidgetHost.deleteAppWidgetId(appWidgetId);
+            }
+        }
+    }
+
+    private void configureWidget(Intent data) 
+    {
+        Bundle extras = data.getExtras();
+        int appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, -1);
+        AppWidgetProviderInfo appWidgetInfo = appWidgetManager.getAppWidgetInfo(appWidgetId);
+        if (appWidgetInfo.configure != null) 
+        {
+            Intent intent = new Intent(AppWidgetManager.ACTION_APPWIDGET_CONFIGURE);
+            intent.setComponent(appWidgetInfo.configure);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            startActivityForResult(intent, REQUEST_CREATE_APPWIDGET);
+        } 
+        else 
+        {
+            createWidget(data);
+        }
+    }
+    
+    
+    public void createWidget(Intent data) 
+    {
+        Bundle extras = data.getExtras();
+        int appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, -1);
+        AppWidgetProviderInfo appWidgetInfo = appWidgetManager.getAppWidgetInfo(appWidgetId);
+        AppWidgetHostView hostView = appWidgetHost.createView(this, appWidgetId, appWidgetInfo);
+        hostView.setAppWidget(appWidgetId, appWidgetInfo);
+        homeView.addView(hostView);
+    }
+    
+    /*The widget should be working now. But if you want to remove the widget, 
+     * you need to ask to the AppWidgetHost to release it. If you do not release it, 
+     * you’ll get a memory leak (your app will consume unnecessary memory). 
+     * Finally, remove it from your LayoutView.
+     */
+    @Override
+    protected void onStart() 
+    {
+        super.onStart();
+        appWidgetHost.startListening();
+    }
+    @Override
+    protected void onStop() 
+    {
+        super.onStop();
+        appWidgetHost.stopListening();
+    }
+    
+    /*The widget should be working now. But if you want to remove the widget, 
+     * you need to ask to the AppWidgetHost to release it. If you do not release it, 
+     * you’ll get a memory leak (your app will consume unnecessary memory). Finally, 
+     * remove it from your LayoutView.
+     */
+    public void removeWidget(AppWidgetHostView hostView) 
+    {
+        appWidgetHost.deleteAppWidgetId(hostView.getAppWidgetId());
+        homeView.removeView(hostView);
+    }
+    /*Note that the widget ID 
+     * is also deleted during the onActivityResult() method 
+     * if the user gave up selecting the widget.
+     */
 
 }
